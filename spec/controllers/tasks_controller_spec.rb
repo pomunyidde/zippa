32f312a29445cc5e5a5e6fd2e73baf1dde05598a
peter omunyidde <pomunyiddde@gmail.com>
2019-10-18 01:15:47 +0000

    task creation API

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -26,6 +26,16 @@ RSpec.describe TasksController, type: :controller do
       task.reload
       expect(task.done).to eq(true)
 	  end
-	end  	
+	end 
+
+	describe "tasks#create" do
+	  it "should allow new tasks to be created" do
+      post :create, params: {task: {title: "Fix things"}}
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq("Fix things")
+      expect(Task.last.title).to eq("Fix things")
+	  end
+	end   	
 
 end
